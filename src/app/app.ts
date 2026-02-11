f7a360b9430a4a909b0f6a37001654d6b09e331f
e-morandi <e.morandi@ufl.edu>
2026-02-11 03:00:30 -0500

    Initial commit, added basic routing + started crude layout of home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  templateUrl: "./app.html",
+  styleUrl: './app.css',
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, MatSidenavModule],
 })
 export class App {
-  protected readonly title = signal('frontend');
+  // protected readonly title = signal('frontend');
+  title = "Title placeholder";
 }
---
+++ src/app/components/stock_page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component 
+({
+    selector: 'stocks',
+    templateUrl: './stock_page.html',
+})
+
+export class stocks {}
