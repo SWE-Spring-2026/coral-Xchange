7323e31ba97d4a57f898e845f75a196fc1b524e1
Carlos-Diaz <cmorales569@gmail.com>
2026-03-24 18:34:33 -0400

    Implemented the log in page for a default user. Now when the user clicks on the user-icon on the top right of the nav bar, it just drops down a menu without changing the page. Then the user can Login as a default user for now. The Sign Up page routing works but is still needs to be implemented.

--- frontend-dev/src/app/auth/sign-up-page/sign-up-page.ts
+++ frontend-dev/src/app/auth/sign-up-page/sign-up-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sign-up-page',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './sign-up-page.html',
   styleUrl: './sign-up-page.css',
 })
--- frontend-dev/src/app/components/user-profile/user-profile.ts
+++ frontend-dev/src/app/components/user-profile/user-profile.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import { Auth } from '../../auth/auth';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, RouterLink, MatButtonModule],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
 })
 export class UserProfile {
+  auth = inject(Auth);
+  private router = inject(Router);
 
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
 }
