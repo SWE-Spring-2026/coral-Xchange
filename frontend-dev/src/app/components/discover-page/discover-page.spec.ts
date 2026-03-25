57c102c1d79dfa81ae717fa51877cf4e21297b12
e-morandi <e.morandi@ufl.edu>
2026-03-25 19:17:47 -0400

    Added test cases, updated home page cards

--- frontend-dev/src/app/components/discover-page/discover-page.spec.ts
+++ frontend-dev/src/app/components/discover-page/discover-page.spec.ts
@@ -20,4 +20,14 @@ describe('DiscoverPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load articles', () => {
+    component.load_selected_news('general');
+    expect(component.news().length).toBeGreaterThan(0);
+  });
+
+  it('should change news type', () => {
+    component.changeNews('forex');
+    expect(component.news_type).toBe('forex');
+  });
 });
